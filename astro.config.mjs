2b2f8bdf6e3c14dd5caf68138644ef70c4d50501
Privitera Stefano Salvatore <s.privitera@campus.unimib.it>
2025-04-09 23:27:46 +0200

    feat: added projects infos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://priviterastefano.github.io',
-    base: '/simple-portfolio',
     compressHTML: true,
     allowImportingTsExtensions: true,
     integrations: [
